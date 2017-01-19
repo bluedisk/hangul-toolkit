f0e0d9b4fb42ddecf9a623fffa2f8baf67dda21a
bluedisk <bluedisk@gmail.com>
2017-01-20 06:22:36 +0900

    Remove python 3.2 from test list

--- setup.py
+++ setup.py
@@ -57,11 +57,11 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: Implementation :: PyPy'
     ],
 
     # What does your project relate to?
